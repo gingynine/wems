37b47da757d37eff2576ec87e77765ff9862c508
gingynine <gingynine@gmail.com>
2016-11-10 15:29:40 -0500

    Morgan
    Command added

--- slackbot_wems/wems-ase/Morgan/slacklib.py
+++ slackbot_wems/wems-ase/Morgan/slacklib.py
@@ -1,5 +1,6 @@
 # Put your commands here
-COMMAND1 = "~~"
+COMMAND1 = "What is my name"
+
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -9,7 +10,7 @@ def handle_command(command):
     """
     response = ""
     if command.find(COMMAND1) >= 0:
-        response = "Huh?"
+        response = "Morgan"
         
     return response
 
